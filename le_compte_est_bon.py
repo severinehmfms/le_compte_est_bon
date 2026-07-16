9f37bf67f0790410045f01a8b8ec9aa018dbfd03
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-16 11:26:35 +0200

    Début de structure de la fonction main_jeu

--- le_compte_est_bon.py
+++ le_compte_est_bon.py
@@ -10,7 +10,25 @@ Séverine Hori Maitrehut
 
 def main_jeu():
     """Fonction principale du jeu le compte est bon"""
+
+    # TODO Tirage au sort du nombre à obtenir
+
+    # TODO Tirage au sort des plaques portant un nombre
+
+    # TODO Tant que conditions de fin de sortie non atteintes: Faire
+
+    # TODO Appel de la fonction affichage des plaques (fonction à écrire)
+
+    # TODO Saisie de l'utilisateur choix d'une opération (fonctions à écrire)
+
+    # TODO Saisie de l'utilisateur choix des nombres parmi les plaques (fonctions à écrire)
+
+    # TODO Calcul demandé par l'utilisateur
+
+
+    # TODO Si sortie de boucle, afficher le dernier nombre atteint
     
+
 
 if __name__ == '__main__':
     main_jeu()
