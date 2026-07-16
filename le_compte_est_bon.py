001d3890b5c945436f5229adb5b95acc5006faeb
Séverine HM <severine.hori.maitrehut@fms-ea.com>
2026-07-16 11:22:27 +0200

    Création de la fonction main, vide pour le moment

---
+++ le_compte_est_bon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+Le compte est bon
+Séverine Hori Maitrehut
+
+"""
+
+def main_jeu():
+    """Fonction principale du jeu le compte est bon"""
+    
+
+if __name__ == '__main__':
+    main_jeu()
